515b85750294903bfbf4a105708b5bf0c5498741
Alex <alexcu21@gmail.com>
2020-07-27 17:35:53 -0600

    simple quote generator

---
+++ script.js
@@ -0,0 +1,80 @@
+// Get Quote from API
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//show loading 
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+
+function complete(){
+    if (!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+async function getQuote(){
+    // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    // const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://quotes.stormconsultancy.co.uk/random.json';
+
+    try{
+        // const response = await fetch(proxyUrl + apiUrl);
+        // const data = await response.json();
+        // console.log(data);
+        loading();
+        const response = await fetch( apiUrl );
+        const data = await response.json();
+        if (data.author === ''){
+            authorText.innerHTML = "Unknow"
+        } else{
+            authorText.innerHTML = data.author;
+        }
+        
+        //reduce font size for long quotes
+        if(data.quote.length > 50){
+            quoteText.classList.add('long-quote');
+        }else{
+            quoteText.classList.remove('long-quote');
+        }
+
+        quoteText.innerHTML = data.quote;
+
+        complete();
+        
+    }catch( error ) {
+        //getQuote();
+        console.log('Oops no quote', error);
+        
+    }
+}
+
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+
+    window.open(twitterUrl, '_blank');
+}
+
+//event Listeners
+
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// on load
+
+// getQuote();
+// loading();
