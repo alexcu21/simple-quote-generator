5f638eeade9c7f9e5328679c12bfd8be67614951
Alex <alexcu21@gmail.com>
2020-07-27 19:32:23 -0600

    adding error messages

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
+const errorMessage = document.getElementById('error-message');
 
 //show loading 
 
-function loading(){
+function showLoadingSpinner(){
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
 // hide loading
 
-function complete(){
+function hideLoadingSpinner(){
     if (!loader.hidden){
         quoteContainer.hidden = false;
         loader.hidden = true;
@@ -27,14 +28,14 @@ async function getQuote(){
     // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     // const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
-     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://quotes.stormconsultancy.co.uk/random.json';
 
     try{
         // const response = await fetch(proxyUrl + apiUrl);
         // const data = await response.json();
         // console.log(data);
-        loading();
+        showLoadingSpinner();
         const response = await fetch( proxyUrl + apiUrl );
         const data = await response.json();
         if (data.author === ''){
@@ -52,10 +53,12 @@ async function getQuote(){
 
         quoteText.innerHTML = data.quote;
 
-        complete();
+        hideLoadingSpinner();
         
     }catch( error ) {
-        getQuote();
+        //getQuote();
+        hideLoadingSpinner();
+        errorMessage.innerHTML = 'Something went wrong, try later';
         console.log('Oops no quote', error);
         
     }
